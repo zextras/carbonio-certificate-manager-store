6338e16f844b4936f7786af961c7be8f44d8c2b7
Davide Frison <davide.frison@zextras.com>
2022-09-16 17:40:11 +0200

    chore(pom.xml): add support for snapshots

--- src/test/java/com/zimbra/cert/VerifyCertKeyTest.java
+++ src/test/java/com/zimbra/cert/VerifyCertKeyTest.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.*;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+
 import com.zimbra.cert.util.ProcessStarter;
 import com.zimbra.common.service.ServiceException;
 import com.zimbra.common.soap.CertMgrConstants;
@@ -17,7 +18,6 @@ import com.zimbra.soap.SoapEngine;
 import com.zimbra.soap.ZimbraSoapContext;
 import java.io.ByteArrayInputStream;
 import java.io.File;
-import java.io.FileReader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -25,7 +25,6 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 import java.util.UUID;
-import java.util.function.Supplier;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -33,28 +32,25 @@ import org.junit.rules.TemporaryFolder;
 
 public class VerifyCertKeyTest {
 
-  @Rule
-  public TemporaryFolder testFolder = new TemporaryFolder();
+  @Rule public TemporaryFolder testFolder = new TemporaryFolder();
 
-  @Rule
-  public ExpectedException exceptionRule = ExpectedException.none();
+  @Rule public ExpectedException exceptionRule = ExpectedException.none();
 
   private String getBaseOperationPath() {
-    return testFolder.getRoot().getAbsolutePath() + File.separator + UUID.randomUUID() + File.separator;
+    return testFolder.getRoot().getAbsolutePath()
+        + File.separator
+        + UUID.randomUUID()
+        + File.separator;
   }
 
   @Test(expected = ServiceException.class)
   public void shouldReturnInvalidIfPvtKeyEmpty() throws Exception {
-    final VerifyCertKey verifyCertKey = new VerifyCertKey(
-        mock(ProcessStarter.class), this::getBaseOperationPath);
+    final VerifyCertKey verifyCertKey =
+        new VerifyCertKey(mock(ProcessStarter.class), this::getBaseOperationPath);
     // prepare request
     Map<String, Object> context = new HashMap<String, Object>();
     ZimbraSoapContext zsc =
-        new ZimbraSoapContext(
-            mock(AuthToken.class),
-            "1",
-            SoapProtocol.Soap12,
-            SoapProtocol.Soap12);
+        new ZimbraSoapContext(mock(AuthToken.class), "1", SoapProtocol.Soap12, SoapProtocol.Soap12);
     context.put(SoapEngine.ZIMBRA_CONTEXT, zsc);
     final XMLElement request = new XMLElement(VERIFY_CERTKEY_REQUEST);
     request.addUniqueElement("cert").addText("test");
@@ -65,22 +61,18 @@ public class VerifyCertKeyTest {
   @Test
   public void shouldReturnTrueIfNoErrorInProcess() throws Exception {
     final ProcessStarter processStarter = mock(ProcessStarter.class);
-    final VerifyCertKey verifyCertKey = new VerifyCertKey(processStarter,
-        this::getBaseOperationPath);
+    final VerifyCertKey verifyCertKey =
+        new VerifyCertKey(processStarter, this::getBaseOperationPath);
     final Process processMock = mock(Process.class);
     when(processStarter.start(any())).thenReturn(processMock);
     when(processMock.waitFor()).thenReturn(1);
-    final ByteArrayInputStream mockProcessResult = new ByteArrayInputStream(
-        "The process went smooth".getBytes(StandardCharsets.UTF_8));
+    final ByteArrayInputStream mockProcessResult =
+        new ByteArrayInputStream("The process went smooth".getBytes(StandardCharsets.UTF_8));
     when(processMock.getInputStream()).thenReturn(mockProcessResult);
     // prepare request
     Map<String, Object> context = new HashMap<String, Object>();
     ZimbraSoapContext zsc =
-        new ZimbraSoapContext(
-            mock(AuthToken.class),
-            "1",
-            SoapProtocol.Soap12,
-            SoapProtocol.Soap12);
+        new ZimbraSoapContext(mock(AuthToken.class), "1", SoapProtocol.Soap12, SoapProtocol.Soap12);
     context.put(SoapEngine.ZIMBRA_CONTEXT, zsc);
     final XMLElement request = new XMLElement(VERIFY_CERTKEY_REQUEST);
     request.addAttribute(CertMgrConstants.E_cert, "test");
@@ -92,22 +84,19 @@ public class VerifyCertKeyTest {
   @Test
   public void shouldReturnFalseIfErrorInProcess() throws Exception {
     final ProcessStarter processStarter = mock(ProcessStarter.class);
-    final VerifyCertKey verifyCertKey = new VerifyCertKey(processStarter,
-        this::getBaseOperationPath);
+    final VerifyCertKey verifyCertKey =
+        new VerifyCertKey(processStarter, this::getBaseOperationPath);
     final Process processMock = mock(Process.class);
     when(processStarter.start(any())).thenReturn(processMock);
     when(processMock.waitFor()).thenReturn(1);
-    final ByteArrayInputStream mockProcessResult = new ByteArrayInputStream(
-        "Error: The process did not go very smooth".getBytes(StandardCharsets.UTF_8));
+    final ByteArrayInputStream mockProcessResult =
+        new ByteArrayInputStream(
+            "Error: The process did not go very smooth".getBytes(StandardCharsets.UTF_8));
     when(processMock.getInputStream()).thenReturn(mockProcessResult);
     // prepare request
     Map<String, Object> context = new HashMap<String, Object>();
     ZimbraSoapContext zsc =
-        new ZimbraSoapContext(
-            mock(AuthToken.class),
-            "1",
-            SoapProtocol.Soap12,
-            SoapProtocol.Soap12);
+        new ZimbraSoapContext(mock(AuthToken.class), "1", SoapProtocol.Soap12, SoapProtocol.Soap12);
     context.put(SoapEngine.ZIMBRA_CONTEXT, zsc);
     final XMLElement request = new XMLElement(VERIFY_CERTKEY_REQUEST);
     request.addAttribute(CertMgrConstants.E_cert, "test");
@@ -117,70 +106,74 @@ public class VerifyCertKeyTest {
   }
 
   /**
-   * When copy/pasting private key or crt files, instead of new lines we receive spaces.
-   * This tests verifies that the returned value is as expected for such inputs.
+   * When copy/pasting private key or crt files, instead of new lines we receive spaces. This tests
+   * verifies that the returned value is as expected for such inputs.
    */
   @Test
   public void shouldMatchExpectedFormatWhenCertificateInputIsValid() {
     final ProcessStarter processStarter = mock(ProcessStarter.class);
-    final VerifyCertKey verifyCertKey = new VerifyCertKey(processStarter,
-        this::getBaseOperationPath);
-    final String testContent = "-----BEGIN CERTIFICATE----- this is my cert "
-        + "-----END CERTIFICATE----- " +
-        "-----BEGIN CERTIFICATE----- another -----END CERTIFICATE-----";
-    final String expectedContent = "-----BEGIN CERTIFICATE-----"
-        + System.lineSeparator()
-        + "this"
-        + System.lineSeparator()
-        + "is"
-        + System.lineSeparator()
-        + "my"
-        + System.lineSeparator()
-        + "cert"
-        + System.lineSeparator()
-        + "-----END CERTIFICATE-----"
-        + System.lineSeparator()
-        + "-----BEGIN CERTIFICATE-----"
-        + System.lineSeparator()
-        + "another"
-        + System.lineSeparator()
-        + "-----END CERTIFICATE-----"
-        + System.lineSeparator();
+    final VerifyCertKey verifyCertKey =
+        new VerifyCertKey(processStarter, this::getBaseOperationPath);
+    final String testContent =
+        "-----BEGIN CERTIFICATE----- this is my cert "
+            + "-----END CERTIFICATE----- "
+            + "-----BEGIN CERTIFICATE----- another -----END CERTIFICATE-----";
+    final String expectedContent =
+        "-----BEGIN CERTIFICATE-----"
+            + System.lineSeparator()
+            + "this"
+            + System.lineSeparator()
+            + "is"
+            + System.lineSeparator()
+            + "my"
+            + System.lineSeparator()
+            + "cert"
+            + System.lineSeparator()
+            + "-----END CERTIFICATE-----"
+            + System.lineSeparator()
+            + "-----BEGIN CERTIFICATE-----"
+            + System.lineSeparator()
+            + "another"
+            + System.lineSeparator()
+            + "-----END CERTIFICATE-----"
+            + System.lineSeparator();
     assertEquals(expectedContent, verifyCertKey.formatValidContent(testContent));
     // transforming the already expected content should have no effect
     assertEquals(expectedContent, verifyCertKey.formatValidContent(expectedContent));
   }
 
   /**
-   * If the input has more than one space in between headers or content,
-   * the result should be different from a proper well-formatted certificate file.
+   * If the input has more than one space in between headers or content, the result should be
+   * different from a proper well-formatted certificate file.
    */
   @Test
   public void shouldNotMatchExpectedContentWhenInputMalformed() {
     final ProcessStarter processStarter = mock(ProcessStarter.class);
-    final VerifyCertKey verifyCertKey = new VerifyCertKey(processStarter,
-        this::getBaseOperationPath);
+    final VerifyCertKey verifyCertKey =
+        new VerifyCertKey(processStarter, this::getBaseOperationPath);
     // Malformed means more than one space
-    final String content = "-----BEGIN CERTIFICATE----- this    is my cert -----END CERTIFICATE----- "
-        + "-----BEGIN CERTIFICATE----- another -----END CERTIFICATE-----";
-    final String expectedContent = "-----BEGIN CERTIFICATE-----"
-        + System.lineSeparator()
-        + "this"
-        + System.lineSeparator()
-        + "is"
-        + System.lineSeparator()
-        + "my"
-        + System.lineSeparator()
-        + "cert"
-        + System.lineSeparator()
-        + "-----END CERTIFICATE-----"
-        + System.lineSeparator() +
+    final String content =
+        "-----BEGIN CERTIFICATE----- this    is my cert -----END CERTIFICATE----- "
+            + "-----BEGIN CERTIFICATE----- another -----END CERTIFICATE-----";
+    final String expectedContent =
         "-----BEGIN CERTIFICATE-----"
-        + System.lineSeparator()
-        + "another"
-        + System.lineSeparator()
-        + "-----END CERTIFICATE-----"
-        + System.lineSeparator();
+            + System.lineSeparator()
+            + "this"
+            + System.lineSeparator()
+            + "is"
+            + System.lineSeparator()
+            + "my"
+            + System.lineSeparator()
+            + "cert"
+            + System.lineSeparator()
+            + "-----END CERTIFICATE-----"
+            + System.lineSeparator()
+            + "-----BEGIN CERTIFICATE-----"
+            + System.lineSeparator()
+            + "another"
+            + System.lineSeparator()
+            + "-----END CERTIFICATE-----"
+            + System.lineSeparator();
     assertFalse(Objects.equals(expectedContent, verifyCertKey.formatValidContent(content)));
   }
 
@@ -188,73 +181,69 @@ public class VerifyCertKeyTest {
   public void shouldMatchChainContentWhenWritingToFile() throws Exception {
     // mock process so request does not break
     final ProcessStarter processStarter = mock(ProcessStarter.class);
-    final VerifyCertKey verifyCertKey = new VerifyCertKey(processStarter,
-        this::getBaseOperationPath);
+    final VerifyCertKey verifyCertKey =
+        new VerifyCertKey(processStarter, this::getBaseOperationPath);
     final Process processMock = mock(Process.class);
     when(processStarter.start(any())).thenReturn(processMock);
     final String caContent = "winds of chain";
     // assert here, because verifycertkey deletes file in mid of call
-    when(processMock.waitFor()).then(x -> {
-      final File caFile = new File(this.getBaseOperationPath() + ZimbraCertMgrExt.COMM_CRT_CA_FILE_NAME);
-      assertEquals(caContent, Files.readString(Path.of(caFile.getAbsolutePath())));
-      return true;
-    });
-    final ByteArrayInputStream mockProcessResult = new ByteArrayInputStream(
-        "The process went smooth".getBytes(StandardCharsets.UTF_8));
+    when(processMock.waitFor())
+        .then(
+            x -> {
+              final File caFile =
+                  new File(this.getBaseOperationPath() + ZimbraCertMgrExt.COMM_CRT_CA_FILE_NAME);
+              assertEquals(caContent, Files.readString(Path.of(caFile.getAbsolutePath())));
+              return true;
+            });
+    final ByteArrayInputStream mockProcessResult =
+        new ByteArrayInputStream("The process went smooth".getBytes(StandardCharsets.UTF_8));
     when(processMock.getInputStream()).thenReturn(mockProcessResult);
 
     // prepare request
     Map<String, Object> context = new HashMap<String, Object>();
     ZimbraSoapContext zsc =
-        new ZimbraSoapContext(
-            mock(AuthToken.class),
-            "1",
-            SoapProtocol.Soap12,
-            SoapProtocol.Soap12);
+        new ZimbraSoapContext(mock(AuthToken.class), "1", SoapProtocol.Soap12, SoapProtocol.Soap12);
     context.put(SoapEngine.ZIMBRA_CONTEXT, zsc);
     final XMLElement request = new XMLElement(VERIFY_CERTKEY_REQUEST);
     request.addUniqueElement(CertMgrConstants.E_cert).addText("test");
     request.addUniqueElement(CertMgrConstants.A_privkey).addText("Super private key");
     request.addUniqueElement(CertMgrConstants.A_ca).addText(caContent);
     verifyCertKey.handle(request, context);
-
   }
 
   @Test
   public void shouldMatchCertContentWhenWritingToFile() throws Exception {
     // mock process so request does not break
     final ProcessStarter processStarter = mock(ProcessStarter.class);
-    final VerifyCertKey verifyCertKey = new VerifyCertKey(processStarter,
-        this::getBaseOperationPath);
+    final VerifyCertKey verifyCertKey =
+        new VerifyCertKey(processStarter, this::getBaseOperationPath);
     final Process processMock = mock(Process.class);
     when(processStarter.start(any())).thenReturn(processMock);
     final String certContent = "Certificate with chain";
     // assert here, because verifycertkey deletes file in mid of call
-    when(processMock.waitFor()).then(x -> {
-      final File caFile = new File(this.getBaseOperationPath() + ZimbraCertMgrExt.COMM_CRT_CA_FILE_NAME);
-      final File certFile = new File(this.getBaseOperationPath() + ZimbraCertMgrExt.COMM_CRT_FILE_NAME);
-      assertEquals(certContent, Files.readString(Path.of(certFile.getAbsolutePath())));
-      assertEquals(certContent, Files.readString(Path.of(caFile.getAbsolutePath())));
-      return true;
-    });
-    final ByteArrayInputStream mockProcessResult = new ByteArrayInputStream(
-        "The process went smooth".getBytes(StandardCharsets.UTF_8));
+    when(processMock.waitFor())
+        .then(
+            x -> {
+              final File caFile =
+                  new File(this.getBaseOperationPath() + ZimbraCertMgrExt.COMM_CRT_CA_FILE_NAME);
+              final File certFile =
+                  new File(this.getBaseOperationPath() + ZimbraCertMgrExt.COMM_CRT_FILE_NAME);
+              assertEquals(certContent, Files.readString(Path.of(certFile.getAbsolutePath())));
+              assertEquals(certContent, Files.readString(Path.of(caFile.getAbsolutePath())));
+              return true;
+            });
+    final ByteArrayInputStream mockProcessResult =
+        new ByteArrayInputStream("The process went smooth".getBytes(StandardCharsets.UTF_8));
     when(processMock.getInputStream()).thenReturn(mockProcessResult);
 
     // prepare request
     Map<String, Object> context = new HashMap<String, Object>();
     ZimbraSoapContext zsc =
-        new ZimbraSoapContext(
-            mock(AuthToken.class),
-            "1",
-            SoapProtocol.Soap12,
-            SoapProtocol.Soap12);
+        new ZimbraSoapContext(mock(AuthToken.class), "1", SoapProtocol.Soap12, SoapProtocol.Soap12);
     context.put(SoapEngine.ZIMBRA_CONTEXT, zsc);
     final XMLElement request = new XMLElement(VERIFY_CERTKEY_REQUEST);
     request.addUniqueElement(CertMgrConstants.E_cert).addText(certContent);
     request.addUniqueElement(CertMgrConstants.A_privkey).addText("Super private key");
     verifyCertKey.handle(request, context);
-
   }
-
 }
